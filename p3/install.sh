0cb95f51cceb4f030d7c82e8ee07834e56ba3cb3
TheWhale01 <itswhale@proton.me>
2025-10-14 13:04:51 +0200

    created install script

---
+++ p3/install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
+
+#
+# INSTALL DOCKER
+#
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo usermod -aG docker ${USER}
+docker run hello-world
+
+#
+# INSTALL KUBECTL
+#
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.34/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.34/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list
+sudo apt update
+sudo apt install kubectl -y
+kubectl version
+
+#
+# INSTALL K3D
+#
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+k3d --version
