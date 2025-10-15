6443a3023f0991078d0d7acd83ef23ebff916bfc
Rossetti <jules.rossetti.pro@gmail.com>
2025-10-15 03:03:53 +0200

    p3 almost working

--- p3/scripts/launch.sh
+++ p3/scripts/launch.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-k3d cluster create argocd --api-port 127.0.0.1:6443 -p "8080:30080@loadbalancer"
+# Create a k3d cluster
+k3d cluster delete argocd || true
+k3d cluster create argocd --api-port 127.0.0.1:6443 -p "8888:80@loadbalancer" -p "8080:443@loadbalancer"
+
+# Install Argo CD
 kubectl create namespace argocd
-kubectl create namespace dev
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl -n argocd patch svc argocd-server -p '{"spec": {"type": "NodePort", "ports": [{"port": 443, "targetPort": 8080, "nodePort": 30080}]}}'
+
+# Wait until Argo CD installation is complete
+kubectl wait --for=condition=Established --timeout=120s crd/applications.argoproj.io crd/appprojects.argoproj.io
+kubectl -n argocd wait --for=condition=Ready pod --all --timeout=180s
+kubectl -n kube-system wait --for=condition=Ready --timeout=120s pod -l app.kubernetes.io/name=traefik
+
+# kubectl -n argocd patch deploy argocd-server \
+#   -p '{"spec":{"template":{"spec":{"containers":[{"name":"argocd-server","args":["--insecure"]}]}}}}'
+
+# Apply the Argo CD Application
+kubectl apply -n argocd -f ./confs/argocd-application.yaml
+# sleep 5
+kubectl apply -n argocd -f ./confs/argocd-ingress.yaml
